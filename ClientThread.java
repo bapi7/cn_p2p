69c5a924505c57a85529bd75318051db98dd0798
hvuradi <nikkivcr@gmail.com>
2017-10-27 15:07:07 -0400

    Modifiesd and removed spaces

--- ClientThread.java
+++ ClientThread.java
@@ -1,5 +1,8 @@
 import java.net.*;
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Vector;
 
 
 public class ClientThread extends Thread
@@ -21,11 +24,8 @@ public class ClientThread extends Thread
 	{
 		
 		this.cfg = cfg;
-		
 		this.requestSocket = s;
-		
 		this.isClient = isClient;
-		
 		util = new TCPMsgUtil();
 		
 		try
@@ -43,67 +43,53 @@ public class ClientThread extends Thread
 	        {
 	        	//sending handshake message to the peer
 	        	this.peerID = peerID;
-	        	
                 util.sendMessage(out, TCPMsgUtil.constructHandshakeMessage(peerID));
-                
                 util.receiveHandshakeMessage(in, Integer.parseInt(peerID));
                 
-	        } 
-	        
+	        } 	        
 	        else 
 	        {
-                
-	        	Integer rcv = util.receiveHandshakeMessage(in, Integer.parseInt(peerID));
-                
+                Integer rcv = util.receiveHandshakeMessage(in, Integer.parseInt(peerID));
 	        	this.peerID = rcv.toString();
-                
 	        	util.sendMessage(out, TCPMsgUtil.constructHandshakeMessage(peerID));
 	        	
 	        }
-
-	        
 	        //Thread to handle bit field and interested/not interested messages
 	        intThread = new Thread() 
 	        {
 	            	
 	        	public void run() 
-	        	{
-	                
-	        		util.sendBitFieldMessage(out, bitField);
-	                
+	        	{	                
+	        		util.sendBitFieldMessage(out, bitField);	                
 	        		byte[] rcv = util.receiveBitFieldMessage(in, cfg.noOfBytes);
 
 	                if(util.isInterested(bitField, rcv))
+	                {
 	                    util.sendInterestedMessage(out);
+	                }
 	                else 
+	                { 
 	                   util.sendNotInterestedMessage(out);
+	                }
 	                
 	        		notify();
 	            }
-
 	        };
 	        
 	        intThread.start();
-
-
-		} 
-		
+		} 		
 		catch(IOException ioe) 
-		{
-			
-			ioe.printStackTrace();
-			
+		{			
+			ioe.printStackTrace();			
 		}
 		
 	}
 
 	public void run() 
-	{
-		
+	{		
 		try 
 		{
 			BufferedInputStream inp_stream = new BufferedInputStream(requestSocket.getInputStream());
-
 			
 		    //while(!stoppingCondition) 
 			 //{
@@ -115,7 +101,7 @@ public class ClientThread extends Thread
 				//MESSAGE = (String)in.readObject();
 			
 				//show the message to the user
-			
+			    // if any messages are 
 				//System.out.println("Receive message: " + MESSAGE);
 			
 			//}
@@ -123,33 +109,22 @@ public class ClientThread extends Thread
 		
 		catch(IOException ioException)
 		{
-			
-			ioException.printStackTrace();
-			
+				ioException.printStackTrace();				
 		} 
 		
 		finally
 		{
 			//Close connections
-			
 			try
-			{
-				
+			{				
 				in.close();
 				out.close();
-				requestSocket.close();
-				
+				requestSocket.close();				
 			} 
 			catch(IOException ioException)
-			{
-				
-				ioException.printStackTrace();
-				
+			{				
+				ioException.printStackTrace();				
 			}
-			
 		}
-		
 	}
-
-
 }
